631ffe7c89de7282b7e09786ccc5cab1a54046c6
Ryuk <hyw511322@gmail.com>
2023-07-04 17:41:17 +0800

    jkhl sorted

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -6,10 +6,10 @@ local opt = {noremap =true, silent =true}
 -- ---------- 插入模式 ---------- ---
 keymap.set("i", "jj", "<ESC>")
 keymap.set("i", "<C-e>", "<End>")
-keymap.set("i", "<C-h>", "<Left>")
-keymap.set("i", "<C-l>", "<Right>")
 keymap.set("i", "<C-j>", "<Down>")
 keymap.set("i", "<C-k>", "<Up>")
+keymap.set("i", "<C-h>", "<Left>")
+keymap.set("i", "<C-l>", "<Right>")
 
 -- ---------- 视觉模式 ---------- ---
 -- 单行或多行移动
@@ -24,10 +24,10 @@ keymap.set("v", "L", "g_")
 -- 窗口
 keymap.set("n", "<leader>sv", "<C-w>v",opt) -- 水平新增窗口 
 keymap.set("n", "<leader>sh", "<C-w>s",opt) -- 垂直新增窗口
-keymap.set("n", "<C-l>", "<C-w>l", opt)
-keymap.set("n", "<C-h>", "<C-w>h", opt)
 keymap.set("n", "<C-j>", "<C-w>j", opt)
 keymap.set("n", "<C-k>", "<C-w>k", opt)
+keymap.set("n", "<C-h>", "<C-w>h", opt)
+keymap.set("n", "<C-l>", "<C-w>l", opt)
 
 -- 取消高亮
 keymap.set("n", "<leader>nh", ":nohl<CR>")
@@ -41,10 +41,10 @@ keymap.set("n", "<leader>nh", ":nohl<CR>")
 keymap.set("n", "j", [[v:count ? 'j' : 'gj']], { noremap = true, expr = true })
 keymap.set("n", "k", [[v:count ? 'k' : 'gk']], { noremap = true, expr = true })
 
-keymap.set("n", "H", "^")
-keymap.set("n", "L", "g_")
 keymap.set("n", "J", "5j")
 keymap.set("n", "K", "5k")
+keymap.set("n", "H", "^")
+keymap.set("n", "L", "g_")
 
 
 -- ---------- OperatorPending ---------- ---
